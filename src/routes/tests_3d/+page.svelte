d10678e3122612f0f7dff40843b518f9e53bfe42
rhoninn11 <rhoninn11@gmail.com>
2024-05-27 12:35:25 +0200

    - tretle bootstrap scene with textured cube

---
+++ src/routes/tests_3d/+page.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+	<title>tests_3d</title>
+	<meta name="description" content="tree js playground" />
+</svelte:head>
+
+<script lang='ts'>
+	import { Canvas } from '@threlte/core';
+	import type { Size } from '@threlte/core';
+	import Scene from 'sdlne/basic_scene/Scene.svelte'
+
+	let general_size: Size = {width: 500, height: 500}
+</script>
+
+
+<div class="text-column">
+	<h1>Here will be some experimention with tree in svelte</h1>
+
+	<Canvas size={{width: 500, height: 500 }}>
+		<Scene />
+	</Canvas>
+	
+</div>
---
+++ src/standalone/basic_scene/Scene.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { T } from '@threlte/core'
+    import { spring } from 'svelte/motion'
+    import { interactivity } from '@threlte/extras'
+    import { useTexture } from '@threlte/extras'
+
+    import textture_file from '$lib/assets/icon.png';
+
+
+    interactivity()
+    const obj_scale = spring(1)
+
+    const generated_image = useTexture(textture_file)
+    // const generated_image = undefined
+
+</script>
+
+<T.PerspectiveCamera
+  makeDefault
+  position={[10, 10, 10]}
+  on:create={({ ref }) => {
+    ref.lookAt(0, 1, 0)
+  }}
+/>
+
+<T.Mesh 
+    scale={$obj_scale}
+    on:pointerenter={() => obj_scale.set(1.5)}
+    on:pointerleave={() => obj_scale.set(1)}
+    on:wheel={(e) => console.log('wheel', e)}
+    position.y={1}>
+    <T.BoxGeometry args={[2, 2, 2]} />
+    {#if $generated_image}
+        <T.MeshBasicMaterial map={$generated_image} />
+    {:else}
+        <T.MeshBasicMaterial color="hotpink" />
+    {/if}
+    
+</T.Mesh>
