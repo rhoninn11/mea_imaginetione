3eb1c2ee53d0fe12c73f71946cf8a9291498b0d7
rhoninn11 <rhoninn11@gmail.com>
2024-05-27 12:35:25 +0200

    - simple circling animation with reactivati

--- src/standalone/xr_scene/Scene.svelte
+++ src/standalone/xr_scene/Scene.svelte
@@ -1,76 +1,60 @@
 <script>
-    import { T } from '@threlte/core'
-    import { spring } from 'svelte/motion'
-    import { interactivity } from '@threlte/extras'
-    import { useTexture, RoundedBoxGeometry, OrbitControls } from '@threlte/extras'
-    import { XR } from '@threlte/xr'
-    import textture_file from '$lib/assets/icon.png';
+	import { T, useTask } from "@threlte/core";
+	import { spring } from "svelte/motion";
+	import { interactivity } from "@threlte/extras";
+	import {
+		useTexture,
+		RoundedBoxGeometry,
+		OrbitControls,
+	} from "@threlte/extras";
+	import { XR } from "@threlte/xr";
+	import textture_file from "$lib/assets/icon.png";
 
+	interactivity();
+	const obj_scale = spring(1);
 
-    interactivity()
-    const obj_scale = spring(1)
+	const generated_image = useTexture(textture_file);
+	// const generated_image = undefined
+	let rotation = 0;
+	let phase = 0;
+	const radius = 1;
 
-    const generated_image = useTexture(textture_file)
-    // const generated_image = undefined
-    let rotation = 0
+	$: pos_on_circle = {
+		x: Math.cos(phase) * radius,
+		y: Math.sin(phase) * radius,
+	};
 
+	useTask((delta) => {
+		rotation += delta;
+		phase += delta;
+	});
 </script>
 
-<!-- <XR/>
-
-
-
-<T.Mesh 
-    scale={$obj_scale}
-    position.y={1.8}
-    position.x={4}>
-    <T.BoxGeometry args={[2, 2, 2]} />
-    {#if $generated_image}
-        <T.MeshBasicMaterial map={$generated_image} />
-    {:else}
-        <T.MeshBasicMaterial color="hotpink" />
-    {/if}
-</T.Mesh> -->
-
-
 <XR />
+
 <T.AmbientLight />
-<T.DirectionalLight
-  position.y={10}
-  position.z={10}
-/>
-<T.PerspectiveCamera
-  makeDefault
-  position.y={1.8}
-  position.z={5}
->
-  <OrbitControls
-    target={[0, 0, 0]}
-    enablePan={false}
-    enableZoom={false}
-  />
+
+<T.DirectionalLight position.y={10} position.z={10} />
+
+<T.PerspectiveCamera makeDefault position.y={1.8} position.z={5}>
+	<OrbitControls target={[0, 0, 0]} enablePan={false} enableZoom={false} />
 </T.PerspectiveCamera>
 
-<T.Mesh
-  position.y={1.8}
-  rotation.x={rotation}
-  rotation.y={rotation}
->
-  <RoundedBoxGeometry args={[0.2, 0.2, 0.2]} />
-  <T.MeshStandardMaterial color="orange" />
+<T.Mesh position.y={1.8} rotation.x={rotation} rotation.y={rotation}>
+	<RoundedBoxGeometry args={[0.2, 0.2, 0.2]} />
+	<T.MeshStandardMaterial color="orange" />
 </T.Mesh>
-<T.Mesh 
-    scale={0.25}
-    position.y={1.8}
-    position.x={1}>
-    <T.BoxGeometry args={[2, 2, 2]} />
-    {#if $generated_image}
-        <T.MeshBasicMaterial map={$generated_image} />
-    {:else}
-        <T.MeshBasicMaterial color="hotpink" />
-    {/if}
+
+<T.Mesh scale={0.25} position.y={1.8+pos_on_circle.y} position.z={pos_on_circle.x} position.x={1}>
+	<T.BoxGeometry args={[2, 2, 2]} />
+	{#if $generated_image}
+		<T.MeshBasicMaterial map={$generated_image} />
+	{:else}
+		<T.MeshBasicMaterial color="hotpink" />
+	{/if}
 </T.Mesh>
+
 <T.Mesh>
-  <T.CylinderGeometry args={[3, 0.01]} />
-  <T.MeshStandardMaterial color="turquoise" />
+	<T.CylinderGeometry args={[3, 0.01]} />
+	<T.MeshStandardMaterial color="turquoise" />
 </T.Mesh>
