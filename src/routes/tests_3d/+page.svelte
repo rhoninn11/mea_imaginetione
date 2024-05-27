e5430c078e5eca9c8d3aa4a4df6556107ea2ec92
rhoninn11 <rhoninn11@gmail.com>
2024-05-27 12:35:26 +0200

    - simple reactivity

--- src/routes/tests_3d/+page.svelte
+++ src/routes/tests_3d/+page.svelte
@@ -9,21 +9,37 @@
 	// import Scene from 'sdlne/basic_scene/Scene.svelte'
 	import Scene from 'sdlne/xr_scene/Scene.svelte'
     import { VRButton } from '@threlte/xr';
+	import CountButton from 'sdlne/components/CountButton.svelte';
 
 	let general_size: Size = {width: 500, height: 500}
 	let is_vr = true
+
+	let count = 0;
+	$: items_repr = Array(count).fill("click");
+
+    function handleClick() {
+        count += 1;
+    }
 </script>
 
 
 <div class="text-column">
-	<h1>Here will be some experimention with tree in svelte</h1>
-
+	<!-- <CountButton/> -->
+	<button on:click={handleClick}>
+		Clicked {count} {count === 1 ? 'time' : 'times'}
+	</button>
 	<Canvas size={{width: 500, height: 500 }}>
 		<Scene />
 	</Canvas>
 	{#if is_vr}
 		<VRButton/>
 	{/if}
+	
+	{#each items_repr as item}
+	<p>
+		jakiś prosty tekst {item}
+	</p>
+	{/each}
 
 	
 </div>
