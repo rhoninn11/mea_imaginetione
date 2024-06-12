3b1c9547aab99ec1958134fa8663fd3ff94da6c5
rhoninn11 <rhoninn11@gmail.com>
2024-06-12 14:38:36 +0200

    - first stylings

---
+++ src/routes/tailwnd/+page.svelte
@@ -0,0 +1,230 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<script>
+	const options = [
+		{label: "option one", txtcls: "text-xs"},
+		{label: "option two", txtcls: "text-bese"},
+		{label: "option three", txtcls: "text-lg"},
+		{label: "option four", txtcls: "text-xl"},
+		{label: "option five", txtcls: "text-2xl"},
+		{label: "option six", txtcls: "text-3xl"},
+	]
+
+</script>
+
+<nav class="sticky top-0 bg-gradient-to-r from-slate-700 to-slate-500 flex items-center">
+	{#each options as item}
+		<a class={`${item.txtcls} relative flex text-zinc-300 p-2 h-full hover:text-zinc-100 hover:bg-white/5
+			transition-colors ease-in-out group`}>
+			<span>{item.label}</span>
+			<div class="group-hover:block hidden absolute top-full right-0 bg-slate-500 whitespace-nowrap p-2">
+				<span>Elo karmelo abakakasaby</span>
+			</div>
+		</a>
+	{/each}
+</nav>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p><p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p><p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p><p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p><p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p><p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p><p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
+
+<p>
+	elo
+</p>
